c431249856446efa070522cb691d17e56c9892eb
Cory G Watson <gphat@onemogin.com>
2017-12-25 09:46:15 -0600

    Next commit

--- cmd/jacquard/main.go
+++ cmd/jacquard/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/stripe/veneur/ssf"
+)
 
 func main() {
+	_, err := bolt.Open("mydb.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	foo := ssf.SSFSpan{}
+
 	fmt.Println("Hello Jacquard")
 }
