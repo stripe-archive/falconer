f30c60554043337a11680863ef00b106a9d245d8
Cory G Watson <gphat@onemogin.com>
2017-12-30 10:34:14 -0600

    Add test and godoc

--- worker.go
+++ worker.go
@@ -8,16 +8,19 @@ import (
 	"github.com/stripe/veneur/ssf"
 )
 
+// Item is a span with an expiration time.
 type Item struct {
 	span       *ssf.SSFSpan
 	expiration int64
 }
 
+// Watch is a streaming query against incoming spans.
 type Watch struct {
 	FoundChan chan *ssf.SSFSpan
 	Tags      map[string]string
 }
 
+// Worker is a repository for spans.
 type Worker struct {
 	SpanChan  chan *ssf.SSFSpan
 	WatchChan chan *ssf.SSFSpan
@@ -25,36 +28,44 @@ type Worker struct {
 	// Intentionally not using pointers because of https://github.com/golang/go/issues/9477
 	// which implies maps without pointers are less GC-fussy since we don't have
 	// traverse the pointer
-	Items      map[int64]Item
-	Watches    map[string]*Watch
-	mutex      sync.Mutex
-	watchMutex sync.Mutex
+	Items              map[int64]Item
+	Watches            map[string]*Watch
+	mutex              sync.Mutex
+	watchMutex         sync.Mutex
+	expirationDuration time.Duration
 }
 
+// NewWorker creates a new worker which stores spans, handles queries and expires
+// old spans.
 func NewWorker(spanDepth int, watchDepth int, expirationDuration time.Duration) *Worker {
 	w := &Worker{
 		// We make our incoming span channel buffered so that we can use non-blocking
 		// writes. This improves write speed by >= 50% but risks dropping spans if
 		// the buffer is full.
-		SpanChan:  make(chan *ssf.SSFSpan, spanDepth),
-		WatchChan: make(chan *ssf.SSFSpan, watchDepth),
-		QuitChan:  make(chan struct{}),
-		Items:     make(map[int64]Item),
-		Watches:   make(map[string]*Watch),
-		mutex:     sync.Mutex{},
+		SpanChan:           make(chan *ssf.SSFSpan, spanDepth),
+		WatchChan:          make(chan *ssf.SSFSpan, watchDepth),
+		QuitChan:           make(chan struct{}),
+		Items:              make(map[int64]Item),
+		Watches:            make(map[string]*Watch),
+		mutex:              sync.Mutex{},
+		watchMutex:         sync.Mutex{},
+		expirationDuration: expirationDuration,
 	}
-	ticker := time.NewTicker(time.Second * 1)
+	ticker := time.NewTicker(expirationDuration)
+	// Use a ticker to periodically expire spans.
 	go func() {
 		for t := range ticker.C {
 			w.Sweep(t.Unix())
 		}
 	}()
+	// Monitor the watch channel and catch any spans that might match.
 	go func() {
 		w.WatchWork()
 	}()
 	return w
 }
 
+// Work monitors the worker's channel and processes any incoming spans.
 func (w *Worker) Work() {
 	for {
 		select {
@@ -66,6 +77,8 @@ func (w *Worker) Work() {
 	}
 }
 
+// WatchWork iterates over watches channel comparing any incoming spans to this
+// workers watches.
 func (w *Worker) WatchWork() {
 	for {
 		select {
@@ -84,13 +97,14 @@ func (w *Worker) WatchWork() {
 	}
 }
 
+// AddSpan adds the span to this worker.
 func (w *Worker) AddSpan(span *ssf.SSFSpan) {
 	w.mutex.Lock()
 	defer w.mutex.Unlock()
 
 	w.Items[span.Id] = Item{
 		span:       span,
-		expiration: time.Now().Add(time.Minute * 1).Unix(),
+		expiration: time.Now().Add(w.expirationDuration).Unix(),
 	}
 
 	if len(w.Watches) > 0 {
@@ -102,6 +116,8 @@ func (w *Worker) AddSpan(span *ssf.SSFSpan) {
 	}
 }
 
+// AddWatch adds a watch with the given name and configures it to return matches
+// to the supplied channel.
 func (w *Worker) AddWatch(name string, tags map[string]string, foundChan chan *ssf.SSFSpan) {
 	w.watchMutex.Lock()
 	defer w.watchMutex.Unlock()
@@ -112,6 +128,7 @@ func (w *Worker) AddWatch(name string, tags map[string]string, foundChan chan *s
 	}
 }
 
+// RemoveWatch removes the watch with the given name.
 func (w *Worker) RemoveWatch(name string) {
 	w.watchMutex.Lock()
 	defer w.watchMutex.Unlock()
@@ -119,6 +136,7 @@ func (w *Worker) RemoveWatch(name string) {
 	delete(w.Watches, name)
 }
 
+// GetTrace returns all spans with the specified trace id.
 func (w *Worker) GetTrace(id int64) []*ssf.SSFSpan {
 	var spans []*ssf.SSFSpan
 	for _, item := range w.Items {
@@ -129,11 +147,11 @@ func (w *Worker) GetTrace(id int64) []*ssf.SSFSpan {
 	return spans
 }
 
+// FindSpans returns all spans matching the specified tags to the channel provided.
 func (w *Worker) FindSpans(tags map[string]string, resultChan chan []*ssf.SSFSpan) {
 	var foundSpans []*ssf.SSFSpan
 	for _, item := range w.Items {
 		span := item.span
-		// scanned++
 		for fk, fv := range tags {
 			if v, ok := span.Tags[fk]; ok {
 				if v == fv {
@@ -147,6 +165,8 @@ func (w *Worker) FindSpans(tags map[string]string, resultChan chan []*ssf.SSFSpa
 	resultChan <- foundSpans
 }
 
+// Sweep deletes any expired spans. The provided time is used for comparisong to
+// facilitate testing.
 func (w *Worker) Sweep(expireTime int64) {
 	w.mutex.Lock()
 	defer w.mutex.Unlock()
@@ -157,5 +177,4 @@ func (w *Worker) Sweep(expireTime int64) {
 			expired++
 		}
 	}
-	// fmt.Printf("Expired %d\n", expired)
 }
---
+++ worker_test.go
@@ -0,0 +1,133 @@
+package falconer
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stripe/veneur/ssf"
+)
+
+func TestWorkerGetTrace(t *testing.T) {
+
+	w := NewWorker(10, 10, time.Second+30)
+
+	traceID := int64(1235)
+
+	span1 := &ssf.SSFSpan{
+		Id:      1234,
+		TraceId: traceID,
+		Tags: map[string]string{
+			"foo": "bar",
+		},
+	}
+	span2 := &ssf.SSFSpan{
+		Id:      1236,
+		TraceId: traceID,
+		Tags: map[string]string{
+			"baz": "gorch",
+		},
+	}
+
+	w.AddSpan(span1)
+	w.AddSpan(span2)
+
+	spans := w.GetTrace(traceID)
+
+	assert.Equal(t, 2, len(spans))
+}
+
+func TestWorkerFindSpans(t *testing.T) {
+
+	w := NewWorker(10, 10, time.Second+30)
+
+	traceID := int64(1235)
+
+	span1 := &ssf.SSFSpan{
+		Id:      1234,
+		TraceId: traceID,
+		Tags: map[string]string{
+			"foo": "bar",
+		},
+	}
+	span2 := &ssf.SSFSpan{
+		Id:      1236,
+		TraceId: traceID,
+		Tags: map[string]string{
+			"baz": "gorch",
+		},
+	}
+
+	w.AddSpan(span1)
+	w.AddSpan(span2)
+
+	resultChan := make(chan []*ssf.SSFSpan, 1)
+	w.FindSpans(map[string]string{"foo": "bar"}, resultChan)
+
+	spans := <-resultChan
+
+	assert.Equal(t, 1, len(spans))
+}
+
+func TestWorkerSweep(t *testing.T) {
+
+	expirationDuration := time.Second * 30
+	w := NewWorker(10, 10, expirationDuration)
+
+	traceID := int64(1235)
+
+	span := &ssf.SSFSpan{
+		Id:      1234,
+		TraceId: traceID,
+		Tags: map[string]string{
+			"foo": "bar",
+		},
+	}
+
+	w.AddSpan(span)
+	// Double the expiration just to be sure
+	then := time.Now().Add(expirationDuration + (time.Second * 1))
+
+	w.Sweep(then.Unix())
+
+	assert.Equal(t, 0, len(w.Items))
+}
+
+func TestWorkerWatches(t *testing.T) {
+
+	w := NewWorker(10, 10, time.Second+30)
+
+	resultChan := make(chan *ssf.SSFSpan, 1)
+	w.AddWatch("farts", map[string]string{"foo": "bar"}, resultChan)
+
+	traceID := int64(1235)
+
+	span1 := &ssf.SSFSpan{
+		Id:      1234,
+		TraceId: traceID,
+		Tags: map[string]string{
+			"foo": "bar",
+		},
+	}
+	span2 := &ssf.SSFSpan{
+		Id:      1236,
+		TraceId: traceID,
+		Tags: map[string]string{
+			"baz": "gorch",
+		},
+	}
+	w.AddSpan(span1)
+	w.AddSpan(span2)
+
+	span := <-resultChan
+
+	assert.Equal(t, int64(1234), span.Id)
+	select {
+	case <-resultChan:
+		assert.Fail(t, "There should be no more spans in the channel")
+	default:
+	}
+
+	w.RemoveWatch("farts")
+	assert.Equal(t, 0, len(w.Watches))
+}
