93bd412b7e7909e6e240b63c6343e2adf27aad12
sam boyer <sdboyer@stripe.com>
2018-05-10 20:33:20 -0400

    Introduce setting for maxage on ingest

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ type Config struct {
 	Debug                         bool   `yaml:"debug"`
 	ListenAddress                 string `yaml:"listen_address"`
 	SpanExpirationDurationSeconds string `yaml:"span_expiration_duration_seconds"`
+	SendSpanConnectionMaxAge      string `yaml:"send_span_connection_max_age"`
 	WorkerCount                   int    `yaml:"worker_count"`
 	WorkerSpanDepth               int    `yaml:"worker_span_depth"`
 	WorkerWatchDepth              int    `yaml:"worker_watch_depth"`
