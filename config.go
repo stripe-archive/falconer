486628b8f830700527b0c2a6fecb40a7b3d1334a
sam boyer <sdboyer@stripe.com>
2018-05-14 14:21:18 -0400

    Reorder config fields

--- config.go
+++ config.go
@@ -2,10 +2,10 @@ package falconer
 
 type Config struct {
 	Debug                         bool   `yaml:"debug"`
-	QueryAddress                  string `yaml:"query_address"`
 	GRPCIngestAddress             string `yaml:"grpc_ingest_address"`
-	SpanExpirationDurationSeconds string `yaml:"span_expiration_duration_seconds"`
+	QueryAddress                  string `yaml:"query_address"`
 	SendSpanConnectionMaxAge      string `yaml:"send_span_connection_max_age"`
+	SpanExpirationDurationSeconds string `yaml:"span_expiration_duration_seconds"`
 	WorkerCount                   int    `yaml:"worker_count"`
 	WorkerSpanDepth               int    `yaml:"worker_span_depth"`
 	WorkerWatchDepth              int    `yaml:"worker_watch_depth"`
