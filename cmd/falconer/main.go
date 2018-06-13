3bf5208f2b61253ea831eac119e195ce6345876c
sam boyer <sdboyer@stripe.com>
2018-06-13 14:34:34 -0400

    Enable gRPC logging via a logrus wrapper

--- cmd/falconer/main.go
+++ cmd/falconer/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stripe/veneur/trace"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 )
 
 var (
@@ -51,6 +52,8 @@ func main() {
 		trace.Service = "falconer"
 	}
 
+	grpclog.SetLoggerV2(logrusWrapper{log})
+
 	falconerServer, err := falconer.NewServer(log, client, &config)
 	if err != nil {
 		log.WithError(err).Fatal("Failed to start server")
@@ -60,4 +63,23 @@ func main() {
 	grpsink.RegisterSpanSinkServer(grpcServer, falconerServer)
 	log.Debug("Falconer started")
 	grpcServer.Serve(lis)
+}
+
+// Wrapper type to allow us to emit grpc logs through logrus.
+type logrusWrapper struct {
+	*logrus.Logger
+}
+
+// V indicates whether a particular log level is at least l. It's required to
+// meet the LoggerV2 interface, but logrus handles such leveling internally, so
+// it's not exposed in this way.  GRPC's logging levels are:
+//
+//   0=info, 1=warning, 2=error, 3=fatal
+//
+// logrus's are:
+//
+//   0=panic, 1=fatal, 2=error, 3=warn, 4=info, 5=debug
+func (lw logrusWrapper) V(l int) bool {
+	logrusLevel := 4 - l
+	return int(lw.Level) <= logrusLevel
 }
