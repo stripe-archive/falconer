fe2be5f42ccfd12db36cf707b86c5a4f0237f8f9
Stephen Jung <sjung@stripe.com>
2018-07-31 12:20:49 -0700

    Support TLS termination in falconer

--- config.go
+++ config.go
@@ -5,7 +5,14 @@ type Config struct {
 	ListenAddress                 string `yaml:"listen_address"`
 	SpanExpirationDurationSeconds string `yaml:"span_expiration_duration_seconds"`
 	StatsdAddress                 string `yaml:"statsd_address"`
-	WorkerCount                   int    `yaml:"worker_count"`
-	WorkerSpanDepth               int    `yaml:"worker_span_depth"`
-	WorkerWatchDepth              int    `yaml:"worker_watch_depth"`
+	TLS                           struct {
+		Certificates []struct {
+			Cert string `yaml:"cert"`
+			Key  string `yaml:"key"`
+		} `yaml:"certificates"`
+		ClientCas []string `yaml:"client_cas"`
+	} `yaml:"tls"`
+	WorkerCount      int `yaml:"worker_count"`
+	WorkerSpanDepth  int `yaml:"worker_span_depth"`
+	WorkerWatchDepth int `yaml:"worker_watch_depth"`
 }
